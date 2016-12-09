d52900629b9fe419bc584c1a05ac5ca5ef063880
Tiago Madeira <tmadeira@gmail.com>
2016-12-09 17:21:28 -0200

    Layout hero
    Added breakpoint-sass, basscss-colors and basscss-background-colors.
    Missing assets: background video and white logo.

--- resources/views/manifesto/index.blade.php
+++ resources/views/manifesto/index.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.default')
 
 @section('content')
+  <section class="hero relative overflow-hidden bg-black white">
+    <video class="hero-video absolute top-0" autoplay="autoplay" loop="loop" poster="">
+      <source src="" type="video/mp4" />
+    </video>
+
+    <h1 class="absolute mx-auto max-width-4 px1 py4 center left-0 right-0">
+      <img alt="Acampamento (inter)nacional das juventudes em luta" src="" />
+    </h1>
+  </section>
+
   <div class="p4 center">
     <p>Em construção.</p>
   </div>

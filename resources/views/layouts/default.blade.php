df77e2a861366340ecd1989ef6bf3228a01b7a76
Tiago Madeira <tmadeira@gmail.com>
2016-12-09 12:26:19 -0200

    Add manifesto.index view

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="mobile-web-app-capable" content="yes" />
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+  <link rel="stylesheet" href="{{ elixir('css/app.css') }}" />
+</head>
+
+<body>
+  @yield('content')
+</body>
+
+</html>

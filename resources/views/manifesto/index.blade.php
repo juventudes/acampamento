0769310f7c88039422ab237c88d0e103a0f6f9fd
Tiago Madeira <tmadeira@gmail.com>
2016-12-10 16:34:42 -0200

    Add logo

--- resources/views/manifesto/index.blade.php
+++ resources/views/manifesto/index.blade.php
@@ -1,14 +1,20 @@
 @extends('layouts.default')
 
 @section('content')
-  <section class="hero relative overflow-hidden bg-black white">
+  <section class="hero relative overflow-hidden bg-orange white">
     <video class="hero-video absolute top-0" autoplay="autoplay" loop="loop" poster="">
       <source src="" type="video/mp4" />
     </video>
 
-    <h1 class="absolute mx-auto max-width-4 px1 py4 center left-0 right-0">
-      <img alt="Acampamento (inter)nacional das juventudes em luta" src="" />
-    </h1>
+    <div class="absolute bottom-0 left-0 right-0 top-0">
+      <div class="table" style="width: 100%; height: 100%;">
+        <div class="table-cell align-middle center px2 py4">
+          <h1 class="hero-logo">
+            <img alt="Acampamento (inter)nacional das juventudes em luta" src="{{ asset('media/logo-white.png') }}" style="max-height: 320px;" />
+          </h1>
+        </div>
+      </div>
+    </div>
   </section>
 
   <div class="p4 center">
